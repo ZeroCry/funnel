5bb805a3fe5578cb73aebe9ad6a4a3e0f3af7d52
Ashish Dubey <ashish.dubey91@gmail.com>
2013-10-16 02:34:00 +0530

    add SCRIPT_NAME in WSGI environ for url prefix

--- website.py
+++ website.py
@@ -1,5 +1,10 @@
 import sys
 import os.path
 sys.path.insert(0, os.path.dirname(__file__))
-from funnel import app as application, init_for
+from funnel import app, init_for
+
+def application(environ, start_response):
+    environ['SCRIPT_NAME'] = '/funnel/'
+    return app(environ, start_response)
+
 init_for('production')
